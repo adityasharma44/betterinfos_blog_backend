b58e9a540c8ffa48ada9ef2c1a39ba97795f14cf
Aditya Sharma <as7007731@gmail.com>
2024-12-04 11:53:16 +0530

    smtp changes for nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,6 +40,9 @@ export const authController = {
 
           let mailTransporter = nodemailer.createTransport({
             service: "gmail",
+            host:"smtp.gmail.com",
+            port:465,
+            secure:true,
             auth: {
               user: process.env.USER,
               pass: process.env.PASS,
